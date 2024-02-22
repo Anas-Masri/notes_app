f8ce3303cf3a18252a7c7fedf50790b0d64a008f
masrianas1994@hotmail.com <masrianas1994@hotmail.com>
2024-02-22 12:01:50 +0300

    create noteItem and chage the font of the app from theme

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/note_item.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -13,6 +14,10 @@ class NotesView extends StatelessWidget {
             height: 25,
           ),
           CustomAppBar(),
+          SizedBox(
+            height: 10,
+          ),
+          NoteItem(),
         ],
       ),
     );
---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        padding:
+            const EdgeInsets.only(left: 16, top: 24, bottom: 24, right: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xfffece84),
+            borderRadius: BorderRadius.circular(18)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(fontSize: 25, color: Colors.black),
+              ),
+              subtitle: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Bulild your career with tharwat samy',
+                    style: TextStyle(
+                        fontSize: 20, color: Colors.black.withOpacity(.5)),
+                  ),
+                ],
+              ),
+              trailing: const FaIcon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 24, top: 10),
+              child: Text(
+                'MAY 21 , 2022',
+                style: TextStyle(
+                    fontSize: 14, color: Colors.black.withOpacity(.5)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
