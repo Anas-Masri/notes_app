8fcd6b3f3674a6c6e3654211e476bc41f1b50d25
masrianas1994@hotmail.com <masrianas1994@hotmail.com>
2024-03-29 15:52:57 +0300

    add color to bottom sheet

--- lib/widgets/color_list_widget.dart
+++ lib/widgets/color_list_widget.dart
@@ -1,36 +1,78 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isSelected, required this.color});
+  final bool isSelected;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        CircleAvatar(
-          backgroundColor: Colors.amber,
-          radius: 40,
-        ),
-        SizedBox(
-          width: 10,
-        )
-      ],
-    );
+    return isSelected
+        ? Row(
+            children: [
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  backgroundColor: color,
+                  radius: 37,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              )
+            ],
+          )
+        : Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 40,
+              ),
+              const SizedBox(
+                width: 10,
+              )
+            ],
+          );
   }
 }
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int selectedIndex = 0;
+  List<Color> colorList = [
+    const Color(0xffec111a),
+    const Color(0xfffb6330),
+    const Color(0xffffd42f),
+    const Color(0xff138468),
+    const Color(0xff009dd6),
+    const Color(0xff7849b8),
+    const Color(0xfff2609e),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 80,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colorList.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              selectedIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              color: colorList[index].withOpacity(.9),
+              isSelected: selectedIndex == index,
+            ),
+          );
         },
       ),
     );
